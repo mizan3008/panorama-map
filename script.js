eb0dca3e61801368722a843ba5a8f0a6a77718ef
Mizanur Rahman <mizan3008@gmail.com>
2022-09-12 16:40:16 +0600

    Add preview and copy payload option

--- script.js
+++ script.js
@@ -6,50 +6,84 @@ const images = []
 let activeIndex = null
 let hotspotId = null
 let viewer = null
+let previewer = null
 
 const hotspots = []
 
 const payload = []
 
+const typeList = ['Unit', 'Level', 'Block']
+
 function _getPayload()
 {
-    // const finalPayload = []
+    const finalPayload = []
 
-    // payload.forEach((data, index) => {
+    payload.forEach((data, index) => {
 
-    //     let nextIndex = index + 1
-    //     if(!payload[nextIndex]){
-    //         nextIndex = 0
-    //     }
+        const toPayload = payload[index + 1] ?? payload[0]
+        const fromPayload = payload[index - 1] ?? payload[payload.length - 1]
 
-    //     finalPayload.push(
-    //         {
-    //             id: index,
-    //             name: index,
-    //             image: data.path,
-    //             pitch: data.pitch,
-    //             yaw: data.yaw,
-    //             toCoordinates: {
-    //                 pitch: data.hotspots[0].pitch ?? 0,
-    //                 yaw: data.hotspots[0].yaw ?? 0,
-    //                 to: nextIndex
-    //             },
-    //             fromCoordinates: {
-    //                 pitch: data.hotspots[1].pitch ?? 0,
-    //                 yaw: data.hotspots[1].yaw ?? 0,
-    //                 to: 0
-    //             },
-    //         }
-    //     )
-    // })
+        let toCoordinates = {
+            pitch: 0,
+            yaw: 0,
+            to: toPayload.name
+        }
 
-    console.log(payload)
-    console.log(viewer.getConfig())
+        let fromCoordinates = {
+            pitch: 0,
+            yaw: 0,
+            to: fromPayload.name
+        }
+
+        const hotspots = data.hotspots ?? []
+
+        if(hotspots.length > 0){
+            hotspots.forEach((hotspot) => {
+                if(hotspot.id.search('from') !== -1){
+                    fromCoordinates.pitch = hotspot.pitch ?? 0
+                    fromCoordinates.yaw = hotspot.yaw ?? 0
+                }
+                if(hotspot.id.search('to') !== -1){
+                    toCoordinates.pitch = hotspot.pitch ?? 0
+                    toCoordinates.yaw = hotspot.yaw ?? 0
+                }
+            })
+        }
+
+        finalPayload.push(
+            {
+                name: data.name,
+                type: data.type,
+                image: data.path,
+                pitch: data.pitch,
+                yaw: data.yaw,
+                toCoordinates: toCoordinates,
+                fromCoordinates: fromCoordinates,
+            }
+        )
+    })
+
+    return finalPayload
 }
+
+function _copy() {
+
+    const payload = _getPayload()
+
+    const input = document.createElement('input');
+    input.setAttribute('value', JSON.stringify(payload));
+    document.body.appendChild(input);
+    input.select();
+    const result = document.execCommand('copy');
+    document.body.removeChild(input);
+    alert('The payload has been copied to the clipboard.')
+    return result;
+ }
+
 function _enableHotspotClickEvent(id)
 {
     hotspotId = id
-    document.getElementById('panorama').addEventListener("click", _clickEvent)    
+    document.getElementById('panorama').addEventListener("click", _clickEvent)
 }
 
 function _clearMouseEvent()
@@ -57,20 +91,26 @@ function _clearMouseEvent()
     document.getElementById('panorama').removeEventListener("click", _clickEvent)
 }
 
+function __getConfig(e)
+{
+    e.preventDefault()
+    payload[activeIndex].pitch = viewer.getPitch()
+    payload[activeIndex].yaw = viewer.getYaw()
+}
+
 function _clickEvent(e)
 {
     e.preventDefault()
-    const hotspots = payload[activeIndex].hotspots ?? []
+
+    const activePayload = payload[activeIndex]
+
+    const hotspots = activePayload.hotspots ?? []
 
     const hotspotPayload = viewer.mouseEventToCoords(e)
-    // console.log(hotspots.indexOf((hp) => hp.id !== hotspotId))
-    // const hotspotId = activeIndex + "-" + hotspots.length
-
-    const text = "Hotspot " + hotspotId
 
     const data = {
         id: hotspotId.toString(),
-        text: text,
+        text: hotspotId.search('from') !== -1 ? 'From Coordinate' : 'To Coordinate',
         pitch: hotspotPayload[0],
         yaw: hotspotPayload[1]
     }
@@ -87,12 +127,10 @@ function _clickEvent(e)
         }
     })
 
-
-    payload[activeIndex].hotspots = uniqueHotspots
+    activePayload.hotspots = uniqueHotspots
+    activePayload.yaw = viewer.getYaw()
+    activePayload.pitch = viewer.getPitch()
     _buildControlPanel()
-
-    // remove duplicate data form payload[activeindex].hotspots
-
     _clearMouseEvent()
 }
 
@@ -116,12 +154,16 @@ function _addImage()
 
         const paths = imagePath.split(',')
         paths.forEach((path) => {
-            payload.push({
-                path: path,
-                pitch: 0,
-                yaw: 0,
-                hotspots: []
-            })
+            if(path !== ""){
+                payload.push({
+                    name: '',
+                    type: 'unit',
+                    path: path,
+                    pitch: 0,
+                    yaw: 0,
+                    hotspots: []
+                })
+            }
         })
 
         if(activeIndex === null){
@@ -150,7 +192,7 @@ function _buildImageList()
     if(payload.length > 0){
         payload.forEach((data, index) => {
             const activeClass = index === activeIndex ? 'border-green-500' : ''
-            html += "<li id='item-image-"+index+"' class='border-2 mb-1 "+ activeClass +" item-images'><div class='flex'><a class='flex' href='javascript:_loadPanorama("+index+")'><div class='mr-3 w-12'><img src='"+data.path+"'/></div><div>Image-"+index+"</div></a><a class='text-red-700' href='javascript:_removeImage("+index+")'><svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-5 h-5'><path stroke-linecap='round' stroke-linejoin='round' d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'/></svg></a></div></li>"
+            html += "<li id='item-image-"+index+"' class='border-2 mb-1 "+ activeClass +" item-images'><div class='flex'><a class='flex' href='javascript:_loadPanorama("+index+")'><div class='mr-3 w-12'><img src='"+data.path+"'/></div><div>Image-"+index+"</div></a><a class='text-red-700 hidden' href='javascript:_removeImage("+index+")'><svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-5 h-5'><path stroke-linecap='round' stroke-linejoin='round' d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'/></svg></a></div></li>"
         })
     }else{
         html += "<li class='border-2'>No images</li>"
@@ -161,6 +203,8 @@ function _buildImageList()
 
 function _removeImage(index)
 {
+    return false
+
     const command = confirm("Are you sure, you want to remove this image?")
     if(command){
         payload.splice(index, 1)
@@ -170,29 +214,59 @@ function _removeImage(index)
     // ToDo: need to handle situation if the active image is removed
 }
 
+function _setName(e)
+{
+    const activePayload = payload[activeIndex]
+    activePayload.name = e.value
+}
+
+function _setType(e)
+{
+    const activePayload = payload[activeIndex]
+    activePayload.type = e.value
+}
+
 function _buildHotspotControl()
 {
+    const activePayload = payload[activeIndex]
+
     const from_coordinate_id = activeIndex + "_from"
     const to_coordinate_id = activeIndex + "_to"
 
+    const name = activePayload.name ?? ''
+
     let html = ""
+
+    let typeOptionHtml = ""
+    typeList.forEach((type) => {
+        let selected = activePayload.type === type.toLocaleLowerCase() ? 'selected' : ''
+        typeOptionHtml += "<option "+selected+" value='"+type.toLocaleLowerCase()+"'>"+type+"</option>"
+    })
+
+    html += "<div class='mt-2'>"
+    html += "<label>Name</label>"
+    html += "<div class='flex'>"
+    html += "<div><input type='text' class='border-2 w-full' onkeyup='javascript:_setName(this);' name='name' value='"+name+"'/></div>"
+    html += "<div><select class='border-2 h-full' onchange='javascript:_setType(this)'>"+typeOptionHtml+"</select></div>"
+    html += "</div>"
+    html += "</div>"
 
     html += "<div class='mt-2'>"
     html += "<div>Hotspot</div>"
     html += "<ul class='list-none'>"
 
-    const hotspots = payload[activeIndex].hotspots ?? []
-
-    if(hotspots.find((hotspot) => hotspot.id === from_coordinate_id) === undefined){
-        html += "<li><a class='bg-gray-400 p-1 text-sm rounded w-full inline-flex items-center' href='javascript:_enableHotspotClickEvent(&#39;"+from_coordinate_id+"&#39;)'><svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-6 h-6 mr-2'><path stroke-linecap='round' stroke-linejoin='round' d='M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zm-7.518-.267A8.25 8.25 0 1120.25 10.5M8.288 14.212A5.25 5.25 0 1117.25 10.5' /></svg> Add from coordinate</a></li>"        
-    }else{
-        html += "<li class='border-2 w-full inline-flex items-center'>From coordinate <a href='javascript:_removeHotspot(&#39;"+from_coordinate_id+"&#39;)'><svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-5 h-5'><path stroke-linecap='round' stroke-linejoin='round' d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'/></svg></a></li>"
-    }
+    const hotspots = activePayload.hotspots ?? []
 
     if(hotspots.find((hotspot) => hotspot.id === to_coordinate_id) === undefined){
-        html += "<li><a class='bg-gray-400 p-1 text-sm rounded w-full inline-flex items-center' href='javascript:_enableHotspotClickEvent(&#39;"+to_coordinate_id+"&#39;)'><svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-6 h-6 mr-2'><path stroke-linecap='round' stroke-linejoin='round' d='M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zm-7.518-.267A8.25 8.25 0 1120.25 10.5M8.288 14.212A5.25 5.25 0 1117.25 10.5' /></svg> Add to coordinate</a></li>"        
+        html += "<li class='mb-1'><a class='bg-gray-400 p-1 text-sm rounded w-full inline-flex items-center' href='javascript:_enableHotspotClickEvent(&#39;"+to_coordinate_id+"&#39;)'><svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-6 h-6 mr-2'><path stroke-linecap='round' stroke-linejoin='round' d='M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zm-7.518-.267A8.25 8.25 0 1120.25 10.5M8.288 14.212A5.25 5.25 0 1117.25 10.5' /></svg> Add to coordinate</a></li>"        
     }else{
-        html += "<li class='border-2 w-full inline-flex items-center'>To coordinate <a href='javascript:_removeHotspot(&#39;"+to_coordinate_id+"&#39;)'><svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-5 h-5'><path stroke-linecap='round' stroke-linejoin='round' d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'/></svg></a></li>"
+        html += "<li class='border-2 w-full inline-flex items-center mb-1'>To coordinate <a class='text-red-500' href='javascript:_removeHotspot(&#39;"+to_coordinate_id+"&#39;)'><svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-5 h-5'><path stroke-linecap='round' stroke-linejoin='round' d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'/></svg></a></li>"
+    }
+
+    if(hotspots.find((hotspot) => hotspot.id === from_coordinate_id) === undefined){
+        html += "<li class='mb-1'><a class='bg-gray-400 p-1 text-sm rounded w-full inline-flex items-center' href='javascript:_enableHotspotClickEvent(&#39;"+from_coordinate_id+"&#39;)'><svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-6 h-6 mr-2'><path stroke-linecap='round' stroke-linejoin='round' d='M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zm-7.518-.267A8.25 8.25 0 1120.25 10.5M8.288 14.212A5.25 5.25 0 1117.25 10.5' /></svg> Add from coordinate</a></li>"        
+    }else{
+        html += "<li class='border-2 w-full inline-flex items-center mb-1'>From coordinate <a class='text-red-500' href='javascript:_removeHotspot(&#39;"+from_coordinate_id+"&#39;)'><svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-5 h-5'><path stroke-linecap='round' stroke-linejoin='round' d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'/></svg></a></li>"
     }
 
     html += "</ul>"
@@ -204,9 +278,6 @@ function _buildHotspotControl()
 function _loadPanorama(index)
 {
     activeIndex = index
-
-    // document.getElementByClassName('item-images').remove('bg-sky-700')
-    // document.getElementById('item-image-'+index).add('bg-sky-700')
 
     _buildControlPanel()
 
@@ -224,4 +295,63 @@ function _loadPanorama(index)
         "yaw": activePayload.yaw ?? 0,
         "hotSpots": activePayload.hotspots ?? []
     })
+
+    document.getElementById('panorama').addEventListener("click", __getConfig)
+}
+
+function _preview()
+{
+    const payload = _getPayload()
+
+    const previewPayload = {   
+        "default": {
+            "firstScene": payload[0].name,
+            "sceneFadeDuration": 1000,
+            "autoLoad": true
+        },
+    
+        "scenes": {}
+    }
+
+    payload.forEach((data) => {
+
+        const hotSpots = []
+
+        if(data.fromCoordinates){
+            hotSpots.push({
+                "pitch": data.fromCoordinates.pitch,
+                "yaw": data.fromCoordinates.yaw,
+                "type": "scene",
+                "sceneId": data.fromCoordinates.to
+            })
+        }
+
+        if(data.toCoordinates){
+            hotSpots.push({
+                "pitch": data.toCoordinates.pitch,
+                "yaw": data.toCoordinates.yaw,
+                "type": "scene",
+                "sceneId": data.toCoordinates.to
+            })
+        }
+
+        previewPayload['scenes'][data.name] = {
+            "pitch": data.pitch,
+            "yaw": data.yaw,
+            "type": "equirectangular",
+            "panorama": data.image,
+            "hotSpots": hotSpots
+        }
+    })
+
+    previewer = pannellum.viewer('panoramicPreview', previewPayload);
+    document.getElementById('previewModal').classList.toggle("hidden")
+}
+
+function toggleModal(){
+    if(previewer){
+        previewer.destroy()
+    }
+
+    document.getElementById('previewModal').classList.toggle("hidden")
 }
