7105c24469f7e79a4887451a32d11a0c602f8a9f
Mizanur Rahman <mizan3008@gmail.com>
2022-09-15 19:30:44 +0600

    Show file name instead of index and auto populate label

--- script.js
+++ script.js
@@ -150,11 +150,15 @@ function _addImage()
         const paths = imagePath.split(',')
         paths.forEach((path) => {
             if(path !== ""){
+                const newPath = path.replace(/(\r\n|\n|\r)/gm, "")
+                const filename = newPath.replace(/^.*[\\\/]/, '')
+
                 payload.push({
+                    filename: filename,
                     name: '',
-                    label: '',
+                    label: filename.replace(/\.[^/.]+$/, ""),
                     type: 'unit',
-                    path: path,
+                    path: newPath,
                     pitch: 0,
                     yaw: 0,
                     hotspots: []
@@ -188,7 +192,7 @@ function _buildImageList()
     if(payload.length > 0){
         payload.forEach((data, index) => {
             const activeClass = index === activeIndex ? 'border-green-500' : ''
-            html += "<li id='item-image-"+index+"' class='border-2 mb-1 "+ activeClass +" item-images'><div class='flex'><a class='flex' href='javascript:_loadPanorama("+index+")'><div class='mr-3 w-12'><img src='"+data.path+"'/></div><div>Image-"+index+"</div></a><a class='text-red-700 hidden' href='javascript:_removeImage("+index+")'><svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-5 h-5'><path stroke-linecap='round' stroke-linejoin='round' d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'/></svg></a></div></li>"
+            html += "<li id='item-image-"+index+"' class='border-2 mb-1 "+ activeClass +" item-images'><div class='flex'><a class='flex' href='javascript:_loadPanorama("+index+")'><div class='mr-3 w-12'><img src='"+data.path+"'/></div><div>"+data.filename+"</div></a><a class='text-red-700 hidden' href='javascript:_removeImage("+index+")'><svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='w-5 h-5'><path stroke-linecap='round' stroke-linejoin='round' d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'/></svg></a></div></li>"
         })
     }else{
         html += "<li class='border-2'>No images</li>"
